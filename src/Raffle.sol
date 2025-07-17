457ffbc71e5bdc694f69fd9e21042a9da74fa3d7
Miguel Mota <mota.m.miguel@gmail.com>
2025-07-17 23:33:40 +0100

    Implement raffle winner picking with VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A sample Raffle contract
@@ -8,31 +10,60 @@ pragma solidity 0.8.19;
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
-    error Raffle__SendMoreToEnterRaffle();
     error Raffle__NotEnoughTimePassedSinceLastRaffle();
+    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
+
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
 
     /* State variables */
+    // Chainlink VRF Variables
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
     // @dev the duration of the lottery in seconds
     uint256 private immutable i_lotteryInterval;
     address payable[] private s_players;
-    uint256 private s_lastTimestamp;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     /* Events */
-    event playerEnteredRaffle (
+    event playerEnteredRaffle(
         address indexed playerAddress,
         uint256 indexed value
     );
+    event WinnerPicked(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
     /* Modifiers */
 
     /* Functions */
-    constructor(uint256 entranceFee, uint256 lotteryInterval) {
+    constructor(
+        uint256 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+        i_gasLane = gasLane;
+        i_lotteryInterval = interval;
+        i_subscriptionId = subscriptionId;
         i_entranceFee = entranceFee;
-        i_lotteryInterval = lotteryInterval;
-        s_lastTimestamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -45,15 +76,47 @@ contract Raffle {
 
     function pickWinner() external {
         // check to see if enough time has passed
-        if (block.timestamp - s_lastTimestamp < i_lotteryInterval) {
+        if (block.timestamp - s_lastTimeStamp < i_lotteryInterval) {
             revert Raffle__NotEnoughTimePassedSinceLastRaffle();
         }
 
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_gasLane,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
 
+        emit RequestedRaffleWinner(requestId);
+    }
 
-        
-
-        s_lastTimestamp = block.timestamp;
+    /**
+     * @dev This is the function that Chainlink VRF node
+     * calls to send the money to the random winner.
+     */
+    function fulfillRandomWords(
+        uint256,
+        /* requestId */ uint256[] calldata randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_players = new address payable[](0);
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(recentWinner);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        // require(success, "Transfer failed");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
     }
 
     /** Getter functions */
